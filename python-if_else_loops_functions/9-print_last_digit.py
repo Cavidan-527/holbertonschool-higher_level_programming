fcfd3f51bfa901789d062ed043aba92260417f7a
Cavidan-527 <13763@holbertonstudents.com>
2026-04-10 16:48:37 +0000

    salam

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    # Ədədin mənfi olub-olmamasından asılı olmayaraq son rəqəmi tapırıq
     last_digit = abs(number) % 10
-    # Son rəqəmi çap edirik (yan-yana çap üçün end="" istifadə etmirik, 
-    # çünki tapşırıq nümunəsində hər çağırış çap olunur)
     print("{}".format(last_digit), end="")
-    # Son rəqəmi geri qaytarırıq (return)
     return last_digit
