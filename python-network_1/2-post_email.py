3d27948d9c6e3fb3143e57b1720a12b72b05686a
Cavidan-527 <13763@holbertonstudents.com>
2026-04-17 09:07:24 +0000

    salam

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -3,24 +3,21 @@
 This script takes in a URL and an email, sends a POST request to the
 passed URL with the email as a parameter, and displays the body.
 """
-import urllib.request
 import urllib.parse
+import urllib.request
 import sys
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    
     # POST məlumatını hazırlayırıq
     values = {'email': email}
     data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')  # Məlumat bayt formatında olmalıdır
-    
+    data = data.encode('ascii')
     # Firewall bypass üçün başlıq əlavə edirik
     headers = {'cfclearance': 'true'}
     req = urllib.request.Request(url, data=data, headers=headers)
-
     with urllib.request.urlopen(req) as response:
         body = response.read()
         print(body.decode('utf-8'))
