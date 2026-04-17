3d492b73277a7f0ed70507e78728ddb46b8e4967
Cavidan-527 <13763@holbertonstudents.com>
2026-04-17 09:08:37 +0000

    salam

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This script fetches https://intranet.hbtn.io/status using the requests package.
+It displays the type and content of the response body.
+"""
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    # Firewall-u keçmək üçün başlıq (header) mütləqdir
+    headers = {'cfclearance': 'true'}
+    
+    response = requests.get(url, headers=headers)
+    
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
